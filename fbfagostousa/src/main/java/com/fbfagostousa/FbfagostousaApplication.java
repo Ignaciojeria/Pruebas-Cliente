6e2601d22e90c80024465bd8750d2f594e91718e
Ignacio <you@example.com>
2018-06-02 18:40:50 -0400

    *Se crea un proyecto Spring con la ayuda de: http://start.spring.io/ *Se utilizan las siguientes dependencias: Web, Jpa, Mysql **Dependencia Web: Disponibiliza end-points **Dependencia Jpa: Proporciona Hibernate (ORM) y Data Jpa para la persistencia y consulta de datos **Mysql: Disponibiliza el driver JDBC para la conexión a bases de datos mysql

---
+++ fbfagostousa/src/main/java/com/fbfagostousa/FbfagostousaApplication.java
@@ -0,0 +1,12 @@
+package com.fbfagostousa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbfagostousaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbfagostousaApplication.class, args);
+	}
+}
