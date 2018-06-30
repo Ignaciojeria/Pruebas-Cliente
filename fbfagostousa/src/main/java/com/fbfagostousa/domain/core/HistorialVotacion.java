c28ce354791cd25860a6d2ed70515fb0d24ae64a
Ignacio <you@example.com>
2018-06-30 04:14:05 -0400

    *Se agregan nuevos atributos

--- fbfagostousa/src/main/java/com/fbfagostousa/domain/core/HistorialVotacion.java
+++ fbfagostousa/src/main/java/com/fbfagostousa/domain/core/HistorialVotacion.java
@@ -1,11 +1,31 @@
 package com.fbfagostousa.domain.core;
 
+import com.fbfagostousa.domain.users.Usuario;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 public class HistorialVotacion {
 
     @Id
     private Long id;
+
+    @OneToOne
+    private Plato plato;
+
+    @OneToOne
+    private Usuario usuario;
+
+    @Column
+    private LocalDate fechaVotacion;
+
+    @Column
+    private LocalTime horaVotacion;
+
+
 }
--- fbfagostousa/src/main/java/com/fbfagostousa/domain/core/Valoracion.java
+++ fbfagostousa/src/main/java/com/fbfagostousa/domain/core/Valoracion.java
@@ -1,21 +1,41 @@
 package com.fbfagostousa.domain.core;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import com.fbfagostousa.domain.users.Usuario;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Valoracion {
 
+    /** La valoracion se hace sobre la caracteristica de la categoria de un plato la a partir de lógica de negocio
+     * podremos ver si la caracteroistica de la categoría de ese plato existe para poder registrar la valoración */
+
     @Id
     private Long id;
 
-    @Column
-    private String nombre;
+    @ManyToOne
+    private Plato plato;
+
+    @ManyToOne
+    private Caracteristica caracteristica;
+
+    @ManyToOne
+    private Usuario usuario;
 
     @Column
-    private Long puntaje;
+    private Long valor;
+
+
+    public Long getValor() {
+        return valor;
+    }
+
+    public void setValor(Long valor) {
+        this.valor = valor;
+    }
+
+
 
     /*
     @ManyToOne
